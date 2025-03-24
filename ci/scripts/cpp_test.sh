d3ee717dde476e08493744ef4885139fa64e46d2
Enrico Minack <github@enrico.minack.dev>
2025-03-24 16:13:09 +0100

    Isolate test process from ctest

--- ci/scripts/cpp_test.sh
+++ ci/scripts/cpp_test.sh
@@ -101,6 +101,7 @@ else
     --parallel ${n_jobs} \
     --repeat until-fail:100 \
     --timeout ${ARROW_CTEST_TIMEOUT:-300} \
+    --force-new-ctest-process \
     -R 'encrypt' \
     "${ctest_options[@]}" \
     "$@"
