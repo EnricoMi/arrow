35724ecda0563c4be679328e1b2375e6744d3c24
Enrico Minack <github@enrico.minack.dev>
2025-03-27 07:13:20 +0100

    Record stacktrace of finishing thread

--- cpp/src/arrow/util/future.cc
+++ cpp/src/arrow/util/future.cc
@@ -23,6 +23,8 @@
 #include <condition_variable>
 #include <mutex>
 #include <numeric>
+#include <iostream>
+#include <boost/stacktrace.hpp>
 
 #include "arrow/util/checked_cast.h"
 #include "arrow/util/config.h"
@@ -125,7 +127,13 @@ class ConcreteFutureImpl : public FutureImpl {
       }
 #endif
 
+      if (IsFutureFinished(state_)) {
+        std::cout << stacktrace << std::endl;
+      }
       DCHECK(!IsFutureFinished(state_)) << "Future already marked finished (" << stateNames[static_cast<int8_t>(state_.load())] << ")";
+      // memorize backtrace of who marked this finished
+      stacktrace = boost::stacktrace::stacktrace();
+
       if (!callbacks_.empty()) {
         callbacks = std::move(callbacks_);
         auto self_inner = shared_from_this();
@@ -197,6 +205,7 @@ class ConcreteFutureImpl : public FutureImpl {
 
   std::mutex mutex_;
   std::condition_variable cv_;
+  boost::stacktrace::basic_stacktrace<> stacktrace;
 };
 
 namespace {
